2a9d6255e47df6866ae8638836bf583eb2d6d6e6
thajucp123 <thajucp123@gmail.com>
2025-08-17 16:05:46 +0530

    UI additions and changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import logo from '../assets/mogam.jpg'
+import cv from '../assets/cv/THAJUDEEN CP - Full Stack Developer - CV 2025.pdf'
 
 function About() {
   return (
     <section id="about" className="about">
+      <img src={logo} alt="Thajudeen's Profile" className="profile-image" />
       <h2>About Me</h2>
-      <p>Hi, I'm <strong>Thajudeen</strong>, a full-stack developer & AI/ML engineer currently working at AES India. With a background in Computer Science, I build applications using the MERN stack, Flutter, and Kotlin. I focus on creating clean, efficient solutions that bridge front-end and back-end seamlessly. Beyond coding, I'm into astronomy, gaming, gym, and photography. 🚀</p>
+      <p>Hi, I'm <strong>Thajudeen</strong>, a <span style={{ backgroundColor: "#325da2", color: "white", padding: "0.25rem 0" }}>&nbsp;Full Stack Developer and AI/ML Engineer&nbsp;</span> with a passion for creating innovative solutions. Currently working at AES India. With a background in Computer Science, I build applications using the MERN stack, AI/ML Solutions, Flutter, and Kotlin. I focus on creating clean, efficient solutions that bridge front-end and back-end seamlessly. Beyond coding, I'm into astronomy, gaming, gym, and photography. 🚀</p>
+      <p>Feel free to connect with me on <a href="https://www.linkedin.com/in/thaju-fakrudheen/" target="_blank" rel="noopener noreferrer" style={{ color: 'var(--blue)', fontWeight: 'bold' }}>LinkedIn</a> or check out my work on <a href="https://github.com/thajucp123" target="_blank" rel="noopener noreferrer" style={{ color: 'var(--blue)', fontWeight: 'bold' }}>GitHub</a>!</p>
+      <div className='about-buttons'>
+      <a href={cv} download className="download-resume-btn">Download Resume</a>
+  <a href="/book" className="read-book-btn">Read My Book</a>
+  </div>
     </section>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,6 +13,19 @@ function Home() {
     <>
       <Navbar />
       <About />
+      {/* section to provide link to previous version of this sit in the Coding Wizard theme. */}
+      {/* this section is devided in two parts: left side is the text and right side is the image (screenshot of previous site). image side is shorter than text side. */}
+      <section className="previous-version">
+      <h2>Check out the previous version of this site</h2>
+      <div className='previous-version-container'>
+        <div className="previous-version-text">
+          <p>This site was previously built with a <span>Coding Wizard</span> theme. You can view the old version <a href="https://wizardry.thajucp.in/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+        </div>
+        <div className="previous-version-image">
+          <img src="https://github.com/thajucp123/personal-portfolio/raw/main/src/assets/img/screenshots/main-screenshot-1.jpg" alt="Previous Version" />
+        </div>
+        </div>
+      </section>
       <Skills />
       <Projects />
       <Experience />
